42f37d3d082a47d285722ac3513a8476199f606b
alexander <yeramtza@gmail.com>
2025-03-11 13:31:19 -0600

    Add initial type definitions and configuration files for various modules

--- Web/frontend/landing-page/src/app/app.routes.ts
+++ Web/frontend/landing-page/src/app/app.routes.ts
@@ -1,21 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { KnowUsComponent } from './pages/know-us/know-us.component';
 
-const routes: Routes = [
+export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'quienes-somos', component: AboutComponent },
     { path: 'proyectos', component: ProjectsComponent },
     { path: 'contacto', component: ContactComponent },
     { path: 'conocenos', component: KnowUsComponent }
 ];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})
-export class AppRoutingModule { }
