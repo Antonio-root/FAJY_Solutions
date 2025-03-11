2af3bfb0fb459de701fbf1412b2798d804719c99
alexander <yeramtza@gmail.com>
2025-03-11 13:04:36 -0600

    Add initial components and routing for landing page

--- Web/frontend/landing-page/src/app/app.routes.ts
+++ Web/frontend/landing-page/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { KnowUsComponent } from './pages/know-us/know-us.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'quienes-somos', component: AboutComponent },
+    { path: 'proyectos', component: ProjectsComponent },
+    { path: 'contacto', component: ContactComponent },
+    { path: 'conocenos', component: KnowUsComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Web/frontend/landing-page/src/app/pages/know-us/know-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-know-us',
+  imports: [],
+  templateUrl: './know-us.component.html',
+  styleUrl: './know-us.component.css'
+})
+export class KnowUsComponent {
+
+}
