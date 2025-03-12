e910ea42357a4c28adf57f2a7876d99052f7ec1c
MANDIN <emanuellgarciaalvarez@gmail.com>
2025-03-12 08:47:47 -0600

    corregi error de terminal

--- Web/frontend/landing-page/src/app/pages/about/about.component.ts
+++ Web/frontend/landing-page/src/app/pages/about/about.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-
 @Component({
   selector: 'app-about',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
